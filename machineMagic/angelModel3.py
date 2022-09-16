12fb60ad00c87148f002bfcf9446ad666f4465f3
geraldyeokj <geraldyeokj@gmail.com>
2022-09-16 10:37:25 -0500

    angels final model inital upload

--- machineMagic/angelModel3.py
+++ machineMagic/angelModel3.py
@@ -1,12 +1,12 @@
-import tensorflow as tf
-import keras
+#import tensorflow as tf
 import numpy as np
 import sys
 from keras.utils import load_img, img_to_array
 from keras import models
+from keras.applications.mobilenet_v2 import preprocess_input
 
 FRUIT = 'banana'
-CLASSES = [' freshripe',' freshunripe',' overripe',' ripe',' rotten',' unripe']
+CLASSES = ['unripe', 'freshunripe', 'freshripe','ripe', 'overripe','rotten']
 IMG_WIDTH, IMG_HEIGHT = 224, 224
 
 # loaded_model = load_model(FRUIT+'_model')
@@ -26,13 +26,18 @@ def preprocess(img_path):
     img = load_img(img_path, target_size = (IMG_WIDTH, IMG_HEIGHT))
     img = img_to_array(img)
     img = np.expand_dims(img, axis = 0)
+    img = preprocess_input(img)
     return img
 
 print("chkpt 222")
-def classify():
-    pred = loaded_model.predict(img)
-    label = np.argmax(pred, axis=-1)
-    name_class = CLASSES[label[0]]
+# def classify():
+#     pred = loaded_model.predict(img)
+#     label = np.argmax(pred, axis=-1)
+#     name_class = CLASSES[label[0]]
+#     return name_class
+def classify(model, img):
+    pred = model.predict(img)
+    name_class = CLASSES[np.argmax(pred)]
     return name_class
 
 print("chkpt 3")
@@ -40,6 +45,6 @@ img_path = sys.argv[1]
 print("chkpt 33")
 img = preprocess(img_path)
 print("chkpt 333")
-name_class = classify()
+name_class = classify(loaded_model, img)
 print("chkpt 3333")
-print("RR:" + name_class + ":RR" )
+print("RR: " + name_class + ":RR" )
